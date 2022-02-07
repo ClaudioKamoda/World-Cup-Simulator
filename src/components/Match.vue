e676843424373cd0f186247458e96bed5fce062d
Claudio <claudiojlf@gmail.com>
2022-02-06 22:27:48 -0300

    finished styling match component

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -1,91 +1,91 @@
 <template>
-  <div class="match">
-    <div class="match__topText">
-      <p><span>00:00</span> - <span>{{stadiumName}}</span></p>
-    </div>
-    <div class="match__data">
-      <img class="match__data--flagA" src="../assets/flags/brazil.png"/>
-      <p class="match__data--teamA">{{teamA}}</p>
-      <p class="match__data--scoreA">#</p>
-      <p class="match__data--cross">x</p>
-      <p class="match__data--scoreB">#</p>
-      <p class="match__data--teamB">{{teamB}}</p>
-      <img class="match__data--flagB" src="../assets/flags/france.png"/>
-    </div>
-  </div>
+	<div class="match">
+		<div class="match__topText">
+			<p>
+				<span>00:00</span> - <span>{{ stadiumName }}</span>
+			</p>
+		</div>
+		<img class="match__flagA" src="../assets/flags/brazil.png" />
+		<p class="match__teamA">{{ teamA }}</p>
+		<p class="match__scoreA">#</p>
+		<p class="match__cross">x</p>
+		<p class="match__scoreB">#</p>
+		<p class="match__teamB">{{ teamB }}</p>
+		<img class="match__flagB" src="../assets/flags/france.png" />
+	</div>
 </template>
 
 <script>
-export default{
-  name: 'Match',
-  data(){
-    return{
-      teamA: 'BRA',
-      teamB: 'FRA',
-      stadiumName: 'Stadium',
-    }
-  }
+export default {
+	name: 'Match',
+	props: {},
+	data() {
+		return {
+			teamA: 'BRA',
+			teamB: 'FRA',
+			stadiumName: 'Stadium'
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
-.match{
-  display: grid;
-  grid-template-rows: 25px 40px;
-  grid-template-columns: 40px 40px 20px 10px 20px 40px 40px;
-  grid-template-areas: 
-    ". stadium stadium stadium stadium stadium ."
-    "flagA teamA scoreA cross scoreB teamB flagB";
-  gap: 5px;
+.match {
+	display: grid;
+	grid-template-rows: 25px 40px;
+	grid-template-columns: 40px 40px 20px 10px 20px 40px 40px;
+	grid-template-areas:
+		'. stadium stadium stadium stadium stadium .'
+		'flagA teamA scoreA cross scoreB teamB flagB';
+	gap: 5px;
 
-  background-color: white;
-  border-radius: 8px;
-  width: fit-content;
-  padding: 5px 15px;
-  font-size: 1.125rem;
+	background-color: white;
+	border-radius: 8px;
+	width: fit-content;
+	padding: 5px 15px;
+	font-size: 1.125rem;
 
-  &__topText{
-    grid-area: stadium;
-    place-self: center;
-  }
-  
-  img{
-    width:40px;
-  }
+	&__topText {
+		grid-area: stadium;
+		place-self: center;
+		font-size: 1rem;
+	}
 
-  &__data
+	img {
+		width: 40px;
+	}
 
-  &__flagA{
-    grid-area: flagA;
-  }
+	&__flagA {
+		grid-area: flagA;
+	}
 
-  &__teamA{
-    grid-area: teamA;
-    place-self: center;
-  }
+	&__teamA {
+		grid-area: teamA;
+		place-self: center;
+	}
 
-  &__scoreA{
-    grid-area: scoreA;
-    place-self: center;
-  }
+	&__scoreA {
+		grid-area: scoreA;
+		place-self: center;
+	}
 
-  &__cross{
-    grid-area: cross;
-    place-self: center;
-  }
+	&__cross {
+		grid-area: cross;
+		place-self: center;
+	}
 
-  &__scoreB{
-    grid-area: scoreB;
-    place-self: center;
-  }
+	&__scoreB {
+		grid-area: scoreB;
+		place-self: center;
+	}
 
-  &__teamB{
-    grid-area: teamB;
-    place-self:center;
-  }
+	&__teamB {
+		grid-area: teamB;
+		place-self: center;
+	}
 
-  &__flagB{
-    grid-area: flagB;
-  }
+	&__flagB {
+		grid-area: flagB;
+	}
 }
 </style>
